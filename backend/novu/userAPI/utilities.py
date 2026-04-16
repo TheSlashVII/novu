6cf667f76a5bcb8fb24d6ad80d182789a1a76570
Junior Uzama <hjmanhacks@gmail.com>
2026-04-16 10:21:15 +0200

    Created file rename Utility

---
+++ backend/novu/userAPI/utilities.py
@@ -0,0 +1,13 @@
+import uuid, os
+from pathlib import Path
+
+BASE_URL=Path(__file__).resolve().parent.parent
+def fileRename(model, file:str, isRegister:bool):
+    if isRegister:
+        fileExtension=file.split('.')[-1] # gets the file extension by searching the first set of letters after the last '.'
+        newname=f"{uuid.uuid4}.{fileExtension}" # generates a new random UUID as a name for the file
+        return os.path.join(f'{BASE_URL}/photos/register_request', newname)
+    return os.path.join(f'{BASE_URL}/photos/user', newname)
+
+    
+
