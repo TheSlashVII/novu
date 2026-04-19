2d05a7a09af2c62e13e6c029a33d31b849e5b15d
duustixx <justinklk866@gmail.com>
2026-04-19 21:54:02 +0200

    Componente studies añadido y cambios en welcome

--- novu/src/app/app.routes.ts
+++ novu/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import {PostRegisterComponent} from './components/post-register/post-register.component';
+import { StudiesComponent } from './components/studies/studies.component';
 
 export const routes: Routes = [
     {
@@ -15,8 +16,13 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
-    }, {
+    }, 
+    {
         path: 'postRegister',
         component: PostRegisterComponent
-  }
+    },
+    {
+        path: 'studies',
+        component: StudiesComponent
+    }
 ];
